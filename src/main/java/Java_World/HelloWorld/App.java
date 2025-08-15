54f2b6a9073e445798dbeed5446f2ab2a56ff1c4
Vamshi Deepak <vamshideepak94@gmail.com>
2025-08-15 11:05:54 +0530

    First Commit of Maven Java Project

---
+++ src/main/java/Java_World/HelloWorld/App.java
@@ -0,0 +1,10 @@
+package Java_World.HelloWorld;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/Java_World/HelloWorld/AppTest.java
@@ -0,0 +1,19 @@
+package Java_World.HelloWorld;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
